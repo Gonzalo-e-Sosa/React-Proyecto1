c705ae6b0f9b5d9d3f57d9c353f7a3c29e2d7731
Gonza <gonzalososa342@gmail.com>
2023-03-10 21:13:18 -0300

    Fin de proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Testimonio1, Testimonio2, Testimonio3} from './componentes/Testimonio.js'
+import Testimonio from './componentes/Testimonio.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen expertos en la programación</h1>
+      </div>
+      <Testimonio {...Testimonio1} />
+      <Testimonio {...Testimonio2} />
+      <Testimonio {...Testimonio3} />
     </div>
   );
 }
---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,26 @@
+export const Testimonio1 = {
+    nombre: 'Karen Trump',
+    pais: 'Estados Unidos',
+    imagen: 'karen',
+    cargo: 'Ingeniera de Software',
+    empresa: 'Google',
+    testimonio: '"Siempre he tenido problemas para aprender JavaScript pero los cursos en linea me ayudaron a consguir mi sueño."'
+}
+
+export const Testimonio2 = {
+    nombre: 'Melina De Luca',
+    pais: 'Argentina',
+    imagen: 'melina',
+    cargo: 'Desarrolladora Web',
+    empresa: 'Spotify',
+    testimonio: '"Desde pequeña me ha gustado el diseño y poder expresarme a través de la programación me llena de felicidad."'
+}
+
+export const Testimonio3 = {
+    nombre: 'Alexa Rodríguez',
+    pais: 'Chile',
+    imagen: 'alexa',
+    cargo: 'Científica de Datos',
+    empresa: 'Binance',
+    testimonio: '"Los algoritmos y los datos me gustan y le dan un sentido a mi trabajo. Soy una gran apasionada de la materia."'
+}
